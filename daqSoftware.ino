0ea8a1bb26665e1ed0ef835368a1b705b108c061
Ryan Meshulam <ryan@sgoblin.com>
2020-02-17 09:39:17 -0800

    Refactored, migrated to STM32 Blue Pill, added DMP

--- daqSoftware.ino
+++ daqSoftware.ino
@@ -1,29 +1,28 @@
-// DAQ 2019
+// DAQ 2020
 // GPL v3
 // SUBC: the UBC Submarine Design Team
 
 #include <Wire.h>
 #include "I2Cdev.h"
-#include "HX711.h"
 #include <SD.h>
 #include <SPI.h>
-#include <MPU6050.h>
+//#include "MPU6050.h"
+#include "MPU6050_6Axis_MotionApps20.h"
+#include "helper_3dmath.h"
 #include <MS5837.h>
 
-// Load cell calibration
-#define calibration_factor -10860.0 //calibrated with 750g test load
-
 // Define which parts are on
-#define LOADCELLON false
-#define TACHOON true
-#define SDON true
+//#define TACHO
+//#define SDON
+//#define DEPTHSENSOR
 
 // Change when plugged in to Arduino
 #define tachoPin 2
 #define BUTTONPIN 3
 #define INDICATORPIN 9
-#define DOUT  5
-#define CLK  6
+#define SDAPIN  PB9
+#define SCLPIN  PB8
+#define IMUINTPIN 2
 
 //#define BEFORECALC 4
 #define MEASUREDELAY 500
@@ -31,13 +30,10 @@
 #define DELAYTIME 5
 #define DEBOUNCETIME 250
 
-// Load cell object
-HX711 scale;
-
 // IMU variables and objects
 MPU6050 IMU;
 bool IMUWorking = false;
-int16_t ax, ay, az, azPrev, oldAX=90;
+int16_t ax, ay, az;
 int16_t gx, gy, gz;
 
 volatile int changes=0;
@@ -46,9 +42,38 @@ unsigned long endTime;
 unsigned long recordTime;
 int duration;
 int oldFall=0;
-boolean running=false;
+boolean running = false;
 //boolean oldRunning=false;
-boolean initialized=false;
+boolean error=false;
+
+// Following code from Jeff Rowberg's I2Cdev example, licensed under MIT license.
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
+
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3];           // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
+
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void dmpDataReady() {
+    mpuInterrupt = true;
+}
+
+// End Rowberg code
+
+#ifdef SDON
+  File outputFile;
+#endif
 
 /*// Debounce stuff
 int buttonState;             // the current reading from the input pin
@@ -62,103 +87,175 @@ void buttonPress(void);
 void tachoChange(void);
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   while (!Serial){
     // Wait for Serial to connect
   }
-  if (SDON==true){
+  
+  // SD Initialization
+  #ifdef SDON
     Serial.print("Initializing SD card...");
     if (!SD.begin(4)) {
-      Serial.println("initialization failed!");
-      while (1);
-    }
+      Serial.println("SD initialization failed!");
+      error = true;
+    } 
     Serial.println("initialization done.");
-  }
+  #endif
 
-  // Initialize IMU
+  // Initialize IMU -- always on
+  Wire.setSDA(SDAPIN);
+  Wire.setSCL(SCLPIN);
   Wire.begin();
   Serial.println( "Initializing the sensor" );
   IMU.initialize ( );
   IMUWorking = IMU.testConnection();
+  pinMode(IMUINTPIN, INPUT);
   if (IMUWorking == true){
     Serial.println ("Successfully Connected");
-    delay (1000);
     Serial.println ( "Taking Values from the sensor" );
-    delay (1000);
+    delay (250);
+    
+    // Start Jeff Rowberg's code, MIT
+    Serial.println("Initializing DMP...");
+    devStatus = IMU.dmpInitialize(); 
+    
+    // supply your own gyro offsets here, scaled for min sensitivity
+    IMU.setXGyroOffset(220);
+    IMU.setYGyroOffset(76);
+    IMU.setZGyroOffset(-85);
+    IMU.setZAccelOffset(1788);
+
+    // make sure it worked (returns 0 if so)
+    if (devStatus == 0) {
+        // Calibration Time: generate offsets and calibrate our MPU6050
+        IMU.CalibrateAccel(6);
+        IMU.CalibrateGyro(6);
+        IMU.PrintActiveOffsets();
+        // turn on the DMP, now that it's ready
+        Serial.println("Enabling DMP...");
+        IMU.setDMPEnabled(true);
+
+        // enable Arduino interrupt detection
+        Serial.print("Enabling interrupt detection (Arduino external interrupt ");
+        Serial.print(digitalPinToInterrupt(IMUINTPIN));
+        Serial.println(")...");
+        attachInterrupt(digitalPinToInterrupt(IMUINTPIN), dmpDataReady, RISING);
+        mpuIntStatus = IMU.getIntStatus();
+
+        // set our DMP Ready flag so the main loop() function knows it's okay to use it
+        Serial.println("DMP ready! Waiting for first interrupt...");
+        dmpReady = true;
+
+        // get expected DMP packet size for later comparison
+        packetSize = IMU.dmpGetFIFOPacketSize();
+    } else {
+        // ERROR!
+        // 1 = initial memory load failed
+        // 2 = DMP configuration updates failed
+        // (if it's going to break, usually the code will be 1)
+        Serial.print("DMP Initialization failed (code ");
+        Serial.print(devStatus);
+        Serial.println(")");
+        error = true;
+    }
+    // End Jeff Rowberg's code
   } else {
     Serial.println("Connection failed");
+    error = true;
   }
   
   // Initialize bar02(MS5837) pressure sensor
-  bar_02.setModel(MS5837::MS5837_02BA);
-  if (!bar_02.init()){
-    Serial.println("Pressure sensor initialization failed!");
-    delay(1000);
-  }
+  #ifdef DEPTHSENSOR
+    bar_02.setModel(MS5837::MS5837_02BA);
+    if (!bar_02.init()){
+      Serial.println("Pressure sensor initialization failed!");
+      error = true;
+      delay(1000);
+    }
+    bool endBuoyancy;
+    MS5837 bar_02; // bar_02 pressure sensor
+    double setDepth;
+    double depth;
 
-  bool endBuoyancy;
-  MS5837 bar_02; // bar_02 pressure sensor
-  double setDepth;
-  double depth;
-  File outputFile; 
+    bar_02.setFluidDensity(997); // kg/m^3 (997 freshwater, 1029 for seawater)
+    bar_02.read();//read pressure sensor
+    setDepth = bar_02.depth(); //get the set depth value
+    // Initialize buoyancy compensator variable
+    endBuoyancy = LOW;
+  #endif
   
-  bar_02.setFluidDensity(997); // kg/m^3 (997 freshwater, 1029 for seawater)
-  bar_02.read();//read pressure sensor
-  setDepth = bar_02.depth(); //get the set depth value
-  // Initialize buoyancy compensator variable
-  endBuoyancy = LOW;
-  pwm = initPWM;
+  #ifdef TACHO
+    attachInterrupt(digitalPinToInterrupt(tachoPin), tachoChange, CHANGE);
+  #endif
+  
+  
   
   // Button stuff
   pinMode(BUTTONPIN, INPUT);
   attachInterrupt(digitalPinToInterrupt(BUTTONPIN), buttonPress, FALLING);
-  if (TACHOON==true){
-    attachInterrupt(digitalPinToInterrupt(tachoPin), tachoChange, CHANGE);
-  }
+  
+  
 
   pinMode(INDICATORPIN, OUTPUT);
   digitalWrite(INDICATORPIN, LOW);
-
-  // Load cell initialization
-  if (LOADCELLON==true){
-    scale.begin(DOUT, CLK);
-    scale.set_scale(calibration_factor); 
-    scale.tare();  //Assuming there is no weight on the scale at start up, reset the scale to 0
-  }
 }
 
 void loop() {
-  if(running==true&&initialized==true&&millis()-startTime>MEASUREDELAY){
+  // Begin Jeff Rowberg code (MIT License)
+  // if programming failed, don't try to do anything
+  if (!dmpReady) return;
+
+  // reset interrupt flag and get INT_STATUS byte
+  mpuInterrupt = false;
+  mpuIntStatus = IMU.getIntStatus();
+
+  // get current FIFO count
+  fifoCount = IMU.getFIFOCount();
+	if(fifoCount < packetSize){
+	        //Lets go back and wait for another interrupt. We shouldn't be here, we got an interrupt from another event
+			// This is blocking so don't do it   while (fifoCount < packetSize) fifoCount = mpu.getFIFOCount();
+	}
+  // check for overflow (this should never happen unless our code is too inefficient)
+  else if ((mpuIntStatus & _BV(MPU6050_INTERRUPT_FIFO_OFLOW_BIT)) || fifoCount >= 1024) {
+    // reset so we can continue cleanly
+    IMU.resetFIFO();
+    //  fifoCount = mpu.getFIFOCount();  // will be zero after reset no need to ask
+    Serial.println(F("FIFO overflow!"));
+  // otherwise, check for DMP data ready interrupt (this should happen frequently)
+  } else if (mpuIntStatus & _BV(MPU6050_INTERRUPT_DMP_INT_BIT)) {
+
+    // read a packet from FIFO
+    while(fifoCount >= packetSize){ // Lets catch up to NOW, someone is using the dreaded delay()!
+      IMU.getFIFOBytes(fifoBuffer, packetSize);
+      // track FIFO count here in case there is > 1 packet available
+      // (this lets us immediately read more without waiting for an interrupt)
+      fifoCount -= packetSize;
+    }
+    // display Euler angles in degrees
+    IMU.dmpGetQuaternion(&q, fifoBuffer);
+    IMU.dmpGetGravity(&gravity, &q);
+    IMU.dmpGetYawPitchRoll(ypr, &q, &gravity);
+    Serial.print("ypr\t");
+    Serial.print(ypr[0] * 180/M_PI);
+    Serial.print("\t");
+    Serial.print(ypr[1] * 180/M_PI);
+    Serial.print("\t");
+    Serial.println(ypr[2] * 180/M_PI);
+  }
+  // End Jeff Rowberg code
+
+  if(!(error) && running && millis()-startTime>MEASUREDELAY){
     endTime=millis();
 
     Serial.print("Time: ");
     duration=(endTime-recordTime);
-    if (SDON==true){
+    #ifdef SDON
       outputFile.print(duration);
-    }
+    #endif
     Serial.print(duration);
     
-    if (TACHOON==true){
-      double RPM = (60000.0*changes/CHANGEPERREV)/(duration);
 
-      if (SDON==true){
-        outputFile.print(",");
-        outputFile.print(RPM);
-      }
-      
-      Serial.print("    RPM: ");
-      Serial.print(RPM);
-    }    
-
-    if (LOADCELLON==true){
-      Serial.print(scale.get_units(), 2); //"scale.get_units(), n" returns a float with n sigfigs
-      Serial.println(" N"); //Units can change but you'll need to refactor the calibration_factor
-
-      if (SDON==true){
-        outputFile.print(",");
-        outputFile.println(scale.get_units());
-      }
-    }
+   
 
     changes = 0;
     startTime=millis();
@@ -172,18 +269,19 @@ void buttonPress(void){
   Serial.println("Button falling!");
   if (millis()-oldFall>DEBOUNCETIME){
     oldFall=millis();
-    if (running==true&&initialized==true){
-      if (SDON==true){
+    if (running==true&&!(error)){
+
+      #ifdef SDON
         // Stuff to close file, etc.
         outputFile.close();
         Serial.println("Closing file...");
-      }
-      initialized=false;
+      #endif
+
       running=false;
       digitalWrite(INDICATORPIN, LOW);
       delay(100);
-    } else if (running==false&&initialized==false){
-      if (SDON==true){
+    } else if (running==false&&!(error)){
+      #ifdef SDON
         // Stuff to open file, etc.
         // create a new file
         char filename[] = "DATA00.CSV";
@@ -197,28 +295,25 @@ void buttonPress(void){
           }
         }
         outputFile.print("Time(ms)");
-        if (TACHOON==true){
+        #ifdef TACHO
           outputFile.print(",RPM");
-        }
-        if (LOADCELLON==true){
-          outputFile.print(",Force(N)");
-        }
+        #endif
         outputFile.println("");
         Serial.print(filename);
         Serial.println(" successfully initialized!");
-      }
+      #endif
+      
       digitalWrite(INDICATORPIN, HIGH);
-      initialized=true;
       running=true;
   
       changes=0;
       startTime=millis();
       recordTime=startTime;
     } else {
-      if (SDON==true){
+      #ifdef SDON
         Serial.println("Something went wrong. Trying to close file.");
         outputFile.close();
-      }
+      #endif
     }
   } else {
     Serial.println("Not enough time has passed since the last event. Debouncing.");
