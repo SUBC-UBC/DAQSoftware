51577be46c4f96d93c634d75eb902869e7610930
artyblouin <72779633+artyblouin@users.noreply.github.com>
2021-03-27 11:16:15 -0700

    Update prototype_worse_implementation.ino

--- prototype_worse_implementation.ino
+++ prototype_worse_implementation.ino
@@ -3,16 +3,17 @@
 #include <Adafruit_NeoPixel.h>
 
 #define PIN 6 // might need to be changed later
-#define NUMPIXELS 7 // number of pixels for the BAD display system
+#define NUMPIXELS 6 // number of pixels for the BAD display system
 
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800); // set up for the pixels, tells the system the number of pixels, the pin they are connected to, the frequency, and the colour space
 
+int LED_instruction = 0; // instruction encoding received from WORSE
+int Overall_instruction = 0;
+int Centre_instruction = 0;
+
 void setup() {
   // put your setup code here, to run once:
-  int LED_instruction = 0; // instruction encoding received from WORSE
-  int Overall_instruction = 0;
-  int Centre_instruction = 0;
-  pixels.begin()
+  pixels.begin();
   pixels.setPixelColor(0, pixels.Color(255, 255, 255)); // Pixel system on currently set to white let me know if youd rather something else
 }
 
@@ -41,7 +42,7 @@ void loop() {
               pixels.setPixelColor(2, pixels.Color(196, 98, 16));
          }
          break;
-   case 3: // I dont know if we need this one
+   /*case 3: // I dont know if we need this one
          if (Overall_instruction / 10 == 2) 
          {
               pixels.setPixelColor(1, pixels.Color(0, 200, 0));
@@ -54,10 +55,10 @@ void loop() {
               pixels.setPixelColor(2, pixels.Color(196, 98, 16));
               pixels.setPixelColor(3, pixels.Color(196, 98, 16));
          }
-         break;
+         break;*/
    default:
          // no LEDs on 
-         for int(i = 1; i < 4; i++){    
+         for (int i = 1; i <= 2; i++){    
               pixels.setPixelColor(i, pixels.Color(0, 0, 0));
          }
          break;
@@ -67,41 +68,42 @@ void loop() {
   switch(Centre_instruction % 10)
   { 
     case 1:
-         if (Overall_instruction / 10 == 1) 
-              pixels.setPixelColor(4, pixels.Color(200, 0, 0));
-         else if (Overall_instruction / 10 == 0)
-              pixels.setPixelColor(4, pixels.Color(255, 255, 0));
+         if (Centre_instruction / 10 == 1) 
+              pixels.setPixelColor(3, pixels.Color(200, 0, 0));
+         else if (Centre_instruction / 10 == 0)
+              pixels.setPixelColor(3, pixels.Color(255, 255, 0));
          break;
     case 2:
-         if (Overall_instruction / 10 == 1) 
+         if (Centre_instruction / 10 == 1) 
          {
+              pixels.setPixelColor(3, pixels.Color(200, 0, 0));
               pixels.setPixelColor(4, pixels.Color(200, 0, 0));
-              pixels.setPixelColor(5, pixels.Color(200, 0, 0));
          }
-         else if (Overall_instruction / 10 == 0)
+         else if (Centre_instruction / 10 == 0)
          {
+              pixels.setPixelColor(3, pixels.Color(255, 255, 0));
               pixels.setPixelColor(4, pixels.Color(255, 255, 0));
-              pixels.setPixelColor(5, pixels.Color(255, 255, 0));
          }
          break;
      case 3:
-         if (Overall_instruction / 10 == 1) 
+         if (Centre_instruction / 10 == 1) 
          {
+              pixels.setPixelColor(3, pixels.Color(200, 0, 0));
               pixels.setPixelColor(4, pixels.Color(200, 0, 0));
               pixels.setPixelColor(5, pixels.Color(200, 0, 0));
-              pixels.setPixelColor(6, pixels.Color(200, 0, 0));
          }
-         else if (Overall_instruction / 10 == 0)
+         else if (Centre_instruction / 10 == 0)
          {
+              pixels.setPixelColor(3, pixels.Color(255, 255, 0));
               pixels.setPixelColor(4, pixels.Color(255, 255, 0));
               pixels.setPixelColor(5, pixels.Color(255, 255, 0));
-              pixels.setPixelColor(5, pixels.Color(255, 255, 0));
          }
-         break;     
+         break;
     default:
-         for int(i = 4; i < 6; i++){    
+         for (int i = 3; i <= 5; i++){    
               pixels.setPixelColor(i, pixels.Color(0, 0, 0));
          }
          break;            
   }
+  pixels.show();
 }
